9738eaee5167ab8d2482670ec1fb0c4f04c1407e
estivs135 <estevan.santos@gmail.com>
2021-05-07 15:30:45 -0300

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+
+
 
 function App() {
   return (
-    <div><h1>Hello World</h1>
+    <>
+    <Navbar/>
+    <div className="container">
+      <h1 className="text-primary">Ola Mundo</h1>
+     <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+
+      <div >
+          <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/estivs135" target="_blank" rel="noreferrer">Estevan</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+      </div>
+      
+    
+    );
+  }
+
+  export default Footer
