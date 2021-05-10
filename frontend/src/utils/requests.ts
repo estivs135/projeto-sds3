19846bcedfc4a34ac1af6b5077677b1937fea833
Estevan Santos <estevan.santos@gmail.com>
2021-05-10 12:09:53 -0300

    Update requests.ts

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';//  MODIFIQUEI PARA A PROXIMA LINHA POIS DESSE JEITO NÃO ESTAVA CONSEGUINDO! MAS VOU CORRIGIR
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-estevan.herokuapp.com'//
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = https://sds3-estevan.herokuapp.com
